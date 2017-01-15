24553e9822ba251aab39c3f8c1d74c7f6d55f03d
Dmytro Firago <d.firago@bellid.com>
2017-01-15 13:09:46 +0100

    Added UPnP auto-configuration script and example interfaces.d with it's usage

---
+++ upnp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# will be returned by wget in case of successful connection
+WGET_SUCCESS_CODE=0
+# will be returned by wget for connection problems not handled by it's retry mechanism
+NETWORK_FAILURE_CODE=4
+# timeout for custom retry mechanism in case of network failure described above, seconds
+NETWORK_FAILURE_TIMEOUT=20
+# relative time when custom retryies will be stopped
+END_TIME=$(( SECONDS + NETWORK_FAILURE_TIMEOUT ))
+
+while
+    wget -q --retry-connrefused --waitretry=2 --tries=20 --spider https://www.duckdns.org && break
+    [[ $? -eq $NETWORK_FAILURE_CODE && $SECONDS -lt $END_TIME ]] # loop continue condition
+do
+    continue
+done
+
+if [ $? -eq $WGET_SUCCESS_CODE ]; then
+    wget -q https://www.duckdns.org/update/jsh-hub1/cc8cc1f9-bac1-4144-9a60-5aba19417328
+    upnpc -e 'SSH on Raspberry Pi' -r 22 TCP > /dev/null
+    upnpc -e 'HTTP Raspberry Pi' -r 8080 TCP > /dev/null
+fi
